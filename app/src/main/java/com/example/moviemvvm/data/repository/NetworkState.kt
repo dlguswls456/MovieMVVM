9ee44fcff4e5bb71ef25df2d77b84c6c1864a98b
HYUNJIN LEE <dlguswls456@naver.com>
2023-11-22 14:16:42 +0900

    Network: 네트워크 상태 클래스 추가

---
+++ app/src/main/java/com/example/moviemvvm/data/repository/NetworkState.kt
@@ -0,0 +1,17 @@
+package com.example.moviemvvm.data.repository
+
+enum class Status {
+    RUNNING,
+    SUCCESS,
+    FAILED
+}
+
+class NetworkState(val status: Status, val msg: String) {
+    //static 같은 개념 하지만 완전 동일하지는 않다
+    //객체 생성하지 않아도 사용 가능하다
+    companion object {
+        val LOADED: NetworkState = NetworkState(Status.SUCCESS, "Success")
+        val LOADING: NetworkState = NetworkState(Status.RUNNING, "Running")
+        val ERROR = NetworkState(Status.FAILED, "Something went wrong")
+    }
+}
