1ec163bdf8f0f70dd5ac8b2b655bbe23e086fa03
HYUNJIN LEE <dlguswls456@naver.com>
2023-11-22 18:00:41 +0900

    Feat: 영화 상세정보 받아오기 기능 추가

--- app/src/main/java/com/example/moviemvvm/MainActivity.kt
+++ app/src/main/java/com/example/moviemvvm/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.moviemvvm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moviemvvm.databinding.ActivityMainBinding
+import com.example.moviemvvm.single_movie_details.SingleMovieActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btn.setOnClickListener {
+            val intent = Intent(this, SingleMovieActivity::class.java)
+            intent.putExtra("id", 299534)
+            this.startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/moviemvvm/single_movie_details/SingleMovieActivity.kt
+++ app/src/main/java/com/example/moviemvvm/single_movie_details/SingleMovieActivity.kt
@@ -2,11 +2,73 @@ package com.example.moviemvvm.single_movie_details
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.moviemvvm.R
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.example.moviemvvm.data.api.POSTER_BASE_URL
+import com.example.moviemvvm.data.api.TheMovieDBClient
+import com.example.moviemvvm.data.api.TheMovieDBInterface
+import com.example.moviemvvm.data.repository.NetworkState
+import com.example.moviemvvm.data.value_object.MovieDetails
+import com.example.moviemvvm.databinding.ActivitySingleMovieBinding
+import java.text.NumberFormat
+import java.util.Locale
 
 class SingleMovieActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySingleMovieBinding
+    private lateinit var viewModel: SingleMovieViewModel
+    private lateinit var movieRepository: MovieDetailsRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_single_movie)
+        binding = ActivitySingleMovieBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val movieId: Int = intent.getIntExtra("id", 1)
+
+        val apiService: TheMovieDBInterface = TheMovieDBClient.getClient()
+        movieRepository = MovieDetailsRepository(apiService)
+
+        viewModel = getViewModel(movieId)
+
+        viewModel.movieDetails.observe(this, Observer {
+            bindUI(it)
+        })
+
+        viewModel.networkState.observe(this, Observer {
+            binding.progressBar.visibility =
+                if (it == NetworkState.LOADING) View.VISIBLE else View.GONE
+            binding.txtError.visibility = if (it == NetworkState.ERROR) View.VISIBLE else View.GONE
+        })
+    }
+
+    private fun bindUI(it: MovieDetails) {
+        binding.movieTitle.text = it.title
+        binding.movieTagline.text = it.tagline
+        binding.movieReleaseDate.text = it.releaseDate
+        binding.movieRating.text = it.rating.toString()
+        binding.movieRuntime.text = it.runtime.toString() + "minutes"
+        binding.movieOverview.text = it.overview
+
+        val formatCurrency = NumberFormat.getCurrencyInstance(Locale.US)
+        binding.movieBudget.text = formatCurrency.format(it.budget)
+        binding.movieRevenue.text = formatCurrency.format(it.revenue)
+
+        val moviePosterURL: String = POSTER_BASE_URL + it.posterPath
+        Glide.with(this)
+            .load(moviePosterURL)
+            .into(binding.ivMoviePoster)
+
+    }
+
+    private fun getViewModel(movieId: Int): SingleMovieViewModel {
+        return ViewModelProvider(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return SingleMovieViewModel(movieRepository, movieId) as T
+            }
+        })[SingleMovieViewModel::class.java]
     }
 }
